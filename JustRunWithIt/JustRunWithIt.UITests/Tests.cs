3c3912c6b5ca4d5ec50765a38905bece9d902870
Johnny Mercado <johnnymercado@live.com>
2015-11-14 12:31:10 -0500

    JustRunWithIt
    Initial Commit

---
+++ JustRunWithIt/JustRunWithIt.UITests/Tests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Android;
+using Xamarin.UITest.Queries;
+
+namespace JustRunWithIt.UITests
+{
+	[TestFixture]
+	public class Tests
+	{
+		AndroidApp app;
+
+		[SetUp]
+		public void BeforeEachTest ()
+		{
+			app = ConfigureApp.Android.StartApp ();
+		}
+
+		[Test]
+		public void EnteringTextAndClickingButtonShouldUpdateHeaderText ()
+		{
+			app.EnterText (c => c.Css ("#textbox"), "Test");
+			app.Tap (c => c.Css ("input[type=button]"));
+
+			AppWebResult[] results = app.WaitForElement (c => c.XPath ("//h1[text()=\"C# says: Test\"]"));
+			app.Screenshot ("Header text updated.");
+
+			Assert.IsTrue (results.Any ());
+		}
+	}
+}
+
