77190c6d8f072e1d39796c21598e24325ef06399
Johnny Mercado <johnnymercado@live.com>
2015-11-14 14:16:49 -0500

    Basic Model Types
    Added User, Group, Event

---
+++ JustRunWithIt/JustRunWithIt/Models/Event.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace JustRunWithIt
+{
+	public enum Category {Sports, Outdoorsy, Social, Running};
+
+	public class Event
+	{
+		public string Name { public get; private set;}
+		public Category EvtCategory { public get; private set; }
+		public Tuple<float, float> Location { public get; private set; }
+		public DateTime StartTime { public get; private set; }
+		public DateTime EndTime {public get; private set; }
+
+		private int id;
+		private int hostid;
+		private string name;
+		private Category evtCategory;
+		private Tuple<float, float> location;
+		private DateTime startTime;
+		private DateTime endTime;
+
+		public Event (string name, float longitude, float latitude, int hostid)
+		{
+			//TODO: Add to sql and retrieve sql identifier
+		
+			this.name = name;
+			this.location.Item1 = longitude;
+			this.location.Item2 = latitude;
+			this.hostid = hostid;
+		}
+
+		public getFromEventID (int eventid)
+		{
+			//TODO: Hydrate model from database
+	}
+}
+
---
+++ JustRunWithIt/JustRunWithIt/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace JustRunWithIt
+{
+	public class User
+	{
+		public string Name { public get; private set; }
+
+		private int id;
+		private string firstName;
+		private string lastName;
+		private string distance;
+
+		public User (string first, string last)
+		{
+			this.firstName = first;
+			this.lastName = last;
+		}
+
+		public static User createFromID(int id){
+			// ToDo: Hook up to database and retrieve info
+		}
+	}
+}
+
