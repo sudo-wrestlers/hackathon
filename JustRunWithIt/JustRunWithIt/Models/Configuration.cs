810405a2b3d79b72c3c604fd45bf70b73dacb759
Johnny Mercado <johnnymercado@live.com>
2015-11-14 19:45:16 -0500

    User Model Hydration
    Added functionality for hydrating User Model from ID

---
+++ JustRunWithIt/JustRunWithIt/Models/Configuration.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Xml;
+
+namespace JustRunWithIt
+{
+	public struct DatabaseInfo{
+		public string Host;
+		public string Username;
+		public string Password;
+		public string Database;
+	}
+
+	public class Configuration
+	{
+		private Configuration (){}
+		public static XmlDocument retrieveConfig(){
+			XmlDocument config = new XmlDocument ();
+			config.PreserveWhitespace = true;
+			config.Load ("config.xml");
+			return config;
+		}
+
+		public static DatabaseInfo retrieveDatabaseInfo(){
+			// Retrieve and parse document
+			XmlDocument config = Configuration.retrieveConfig();
+			XmlNode root = config.FirstChild;
+			XmlNode databaseInfo = root.SelectSingleNode ("database");
+
+			// Create data struct and hydrate
+			DatabaseInfo db = new DatabaseInfo ();
+			db.Host = databaseInfo.SelectSingleNode ("host").Value;
+			db.Username = databaseInfo.SelectSingleNode ("username").Value;
+			db.Password = databaseInfo.SelectSingleNode ("password").Value;
+			db.Database = databaseInfo.SelectSingleNode ("database_name").Value;
+
+			return db;
+		}
+	}
+}
+
--- JustRunWithIt/JustRunWithIt/Models/User.cs
+++ JustRunWithIt/JustRunWithIt/Models/User.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Data.SqlClient;
+using System.Xml;
+using System.Data;
 
 namespace JustRunWithIt
 {
@@ -20,12 +22,38 @@ namespace JustRunWithIt
 			this.lastName = last;
 		}
 
+		/**
+		 * Retrieve a hydrated User object from the database. Receive an empty model
+		 * on failure
+		 */
 		public static User createFromID(int id){
 			User user = new User ();
+		
+			// Get Config File
+			DatabaseInfo config = Configuration.retrieveDatabaseInfo();
 
+			// Connect to DB
 			SqlConnection db = new SqlConnection();
+			db.ConnectionString = "Data Source: " + config.Host + ";"
+								+ "Initial Catalog: " + config.Database + ";"
+								+ "User ID: " + config.Username + ";"
+								+ "Password: " + config.Password + ";";
 
+			// Execute command to retrieve data
+			SqlCommand query = new SqlCommand ();
+			query.CommandText = "SELECT * FROM Users Where UserID = @USERID";
+			query.Parameters.Add ("@USERID", SqlDbType.Int);
+			query.Parameters ["@USERID"].Value = id;
 
+			try{
+				SqlDataReader data = query.ExecuteReader ();
+				user.id = data.GetInt32(data.GetOrdinal("UserID"));
+				user.firstName = data.GetString(data.GetOrdinal("FirstName"));
+				user.lastName = data.GetString(data.GetOrdinal("LastName"));
+				user.distance = data.GetFloat(data.GetOrdinal("Distance"));
+			} catch (Exception dbExecute) {
+				Console.WriteLine (dbExecute.Message);
+			}
 			return user;
 		}
 	}
